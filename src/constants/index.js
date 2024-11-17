1f887640e553cc51fb4b8f40ba3b3c731ec1f1dc
II-777 <45958058+II-777@users.noreply.github.com>
2024-11-16 20:43:52 -0500

    add task 1, 2, 3, 4 (pwd reset email)

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 // src/constants/index.js
+import path from 'node:path';
 
 // Sorting constants
 export const SORT_ORDER = {
@@ -9,3 +10,15 @@ export const SORT_ORDER = {
 // Authentication session constants
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+// SMTP config constants
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+// Handlebars constants
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,19 @@
 // src/routers/auth.js
 import { Router } from 'express';
 import { 
-  registerUserController, 
-  loginUserController, 
-  logoutUserController, 
-  refreshUserSessionController 
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+  requestResetEmailController,
+  resetPasswordController,
 } from '../controllers/auth.js';
-import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
+import {
+  registerUserSchema,
+  loginUserSchema,
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -27,5 +34,17 @@ router.post(
 router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+router.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+
+router.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
 
 export default router;
